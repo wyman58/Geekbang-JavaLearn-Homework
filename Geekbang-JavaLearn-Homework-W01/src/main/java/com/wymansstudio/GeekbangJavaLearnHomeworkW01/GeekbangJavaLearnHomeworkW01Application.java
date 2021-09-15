b2419ca0bbb15016ff0b78ce4ec8514782d94f7e
Wyman Chen <wyman.chen@wymansstudio.com>
2021-09-15 23:55:45 +1000

    Project Init

---
+++ Geekbang-JavaLearn-Homework-W01/src/main/java/com/wymansstudio/GeekbangJavaLearnHomeworkW01/GeekbangJavaLearnHomeworkW01Application.java
@@ -0,0 +1,13 @@
+package com.wymansstudio.GeekbangJavaLearnHomeworkW01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekbangJavaLearnHomeworkW01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekbangJavaLearnHomeworkW01Application.class, args);
+	}
+
+}
